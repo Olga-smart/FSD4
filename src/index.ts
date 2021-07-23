03005488a6ee021df0acebaacf4216adb47ed833
olga-smart <olgat.smart@gmail.com>
2021-07-23 23:14:27 +0700

    Isolate variables into separate namespace

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,29 @@ import {Model} from './model/model';
 import {View} from './view/view';
 import {Presenter} from './presenter/presenter';
 
-let sliders = document.querySelectorAll('.js-range-slider');
-for (let slider of sliders) {
-  let model: Model = new Model({
-    // min: 10,
-    // max: 150,
-    // leftValue: -100,
-    // rightValue: -50,
-    range: true
-  });
-  
-  let view = new View(slider, {
-    minMaxLabels: true,
-    valueLabel: true,
-    // vertical: true,
-    range: true,
-    scale: true,
-    scaleIntervals: 5
-  });
-  
-  let presenter = new Presenter(model, view);
-  
-  view.registerWith(presenter);
-}
+(function() {
+  let sliders = document.querySelectorAll('.js-range-slider');
+  for (let slider of sliders) {
+    let model: Model = new Model({
+      // min: 10,
+      // max: 150,
+      // leftValue: -100,
+      // rightValue: -50,
+      range: true
+    });
+    
+    let view = new View(slider, {
+      minMaxLabels: true,
+      valueLabel: true,
+      // vertical: true,
+      range: true,
+      scale: true,
+      scaleIntervals: 5
+    });
+    
+    let presenter = new Presenter(model, view);
+    
+    view.registerWith(presenter);
+  }
+}());
+
